8551e73b96cecaba7814cc1316a7c9993edcc941
LuisSilva2014 <l.r.s.m.2013@gmail.com>
2016-01-18 14:01:13 -0500

    Tutoriales desde el 1 al 6

---
+++ EjemploAngularJS/Sapp/js/controlador.js
@@ -0,0 +1,25 @@
+﻿angular.module("MiprimerModulo", [])
+    .controller("PrimerControlador", function ($scope) {
+        $scope.nombre = "Luis silva";
+        $scope.nuevoComentario = {};
+        $scope.comentarios = [
+            {
+                comentario: "Buen comentario",
+                usuario: "luissilva"
+            },
+            {
+                comentario: "Buen comentario2",
+                usuario: "luissilva2"
+            }
+        ];
+        $scope.agregarComentario = function () {
+            //Two way databing
+            $scope.comentarios.push($scope.nuevoComentario);
+            $scope.nuevoComentario = {};
+                
+            //para reiniciar el sistema de comentarios se agrega o siguiente:
+        }
+    });
+
+
+
