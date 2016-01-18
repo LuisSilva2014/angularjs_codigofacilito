aa0a0ac21d317782fc02204aed5503e7fcb0df04
LuisSilva2014 <l.r.s.m.2013@gmail.com>
2016-01-18 14:02:44 -0500

    Tutorial 6

--- EjemploAngularJS/Sapp/js/controlador.js
+++ EjemploAngularJS/Sapp/js/controlador.js
@@ -1,8 +1,10 @@
-﻿angular.module("MiprimerModulo", [])
-    .controller("PrimerControlador", function ($scope) {
-        $scope.nombre = "Luis silva";
-        $scope.nuevoComentario = {};
-        $scope.comentarios = [
+﻿
+//* INYECCION DE DEPENDENCIAS
+angular.module("MiprimerModulo", [])
+    .controller("PrimerControlador", ["$scope", function (m) {
+        m.nombre = "Luis silva";
+        m.nuevoComentario = {};
+        m.comentarios = [
             {
                 comentario: "Buen comentario",
                 usuario: "luissilva"
@@ -12,14 +14,11 @@
                 usuario: "luissilva2"
             }
         ];
-        $scope.agregarComentario = function () {
+        m.agregarComentario = function () {
             //Two way databing
-            $scope.comentarios.push($scope.nuevoComentario);
-            $scope.nuevoComentario = {};
-                
+            m.comentarios.push($scope.nuevoComentario);
+            m.nuevoComentario = {};
+
             //para reiniciar el sistema de comentarios se agrega o siguiente:
         }
-    });
-
-
-
+    }]);
