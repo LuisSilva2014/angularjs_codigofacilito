12da2330c68de1cad1f731b74aca1fce1f95ae8b
LuisSilva2014 <l.r.s.m.2013@gmail.com>
2016-01-18 15:22:32 -0500

    9- To do List

--- EjemploAngularJS/Sapp/js/controlador.js
+++ EjemploAngularJS/Sapp/js/controlador.js
@@ -1,34 +1,31 @@
 ﻿
-//* 7- 8.- Curso AngularJS - AJAX con $http
-angular.module("MiprimerModulo", [])
-    .controller("PrimerControlador",function ($scope, $http) {
-        $scope.publicaciones=[];
-        $scope.newPost ={};
+//* 9- To do List
+var app = angular.module("MiprimerModulo", ["LocalStorageModule"])
+.controller("PrimerControlador", function ($scope, localStorageService) {
+    //primero verifamos si tenemos algon el el localstorege atraves de la clave angular-myToDoList
+    if (localStorageService.get("angular-myToDoList")) {
+        $scope.toDo = localStorageService.get("angular-myToDoList");
+    }
+    else{
+        $scope.toDo = [];
+    }
+    //para hacer refactory del codigo es utilizando el metodo Watch
+    $scope.$watchCollection('toDo',function(newValue,oldValue){
+        localStorageService.set("angular-myToDoList",$scope.toDo)    ;
+    });
+    $scope.addActivity = function(){
+        $scope.toDo.push($scope.newActivity);
+        $scope.newActivity = {};
 
-        $http.get("http://jsonplaceholder.typicode.com/posts")
-            .success(function(data){
-                console.log(data);
-                $scope.publicaciones = data;
+        //cada vez que se hace la modificacion de la lista se debe actulizar localStorageService
 
-            }
-            
-            ).error (function(err){
-            
-            });
+        //   localStorageService.set("angular-myToDoList",$scope.toDo); Al tener el wath nos ahorramos esta línea
+        //angular-myToDoList es mi lista por hacer que va a ser buscada en el localstorage para agregar o mostrar listas de tareas. / angular-myToDoList is my list to do to be sought in the localStorage to add or display lists.
+    }
+    $scope.clean = function(){
+        $scope.toDo = [];
+        // localStorageService.set("angular-myToDoList",$scope.toDo); Al tener el wath nos ahorramos esta línea
+    }    //Tambien se puede optimizar agregandolo en el html al boton que llama clean asi
+    //<butto ng-click="toDo =[]">
+});
 
-        $scope.addPost = function(){
-            $http.post("http://jsonplaceholder.typicode.com/posts",{
-                title:$scope.newPost.tite,
-                body: $scope.newPost.body,
-                userId: 1
-            })
-            .success(function(data, status, headers, config){
-                console.log(data);
-                $scope.publicaciones.push($scope.newPost);
-                $scope.newPost ={};
-            }        
-            ).error (function(error, status, headers, config){
-                console.log(error)
-            });
-        }
-    });;
